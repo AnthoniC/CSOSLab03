ecb8d829c1aeaf7949c0ff7ed13d2ab188e4ecea
Aaron Block <ablock@austincollege.edu>
2019-01-31 14:15:09 -0600

    Initial Commit

---
+++ MiniOS/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MiniOS
+//
+//  Created by Aaron David Block on 1/31/19.
+//  Copyright © 2019 Austin College. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
